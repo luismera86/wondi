c199734ac35e2d823e1aa478d62660767cd23fd3
Luis Mera <luismera86@gmail.com>
2022-06-08 18:13:36 -0300

    carousel completo

--- js/index.js
+++ js/index.js
@@ -6,7 +6,6 @@ window.addEventListener('load', function () {
 	new Glider(document.querySelector('.carousel__lista'), {
 		slidesToShow: 1,
 		slidesToScroll: 1,
-		draggable: true,
 		dots: '.carousel__indicadores',
 		arrows: {
 			prev: '.carousel__anterior',
