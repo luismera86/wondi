95729deb497b2fcaf352127029633832ec4d24de
Luis Mera <luismera86@gmail.com>
2022-06-08 15:26:45 -0300

    pruebas de carousel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,35 @@
-document.getElementById('menu').onclick = () => menu.classList.remove('show');
+document.getElementById('menu').onclick = () => menu.classList.remove('show')
+
+// Carousel
+
+window.addEventListener('load', function () {
+	new Glider(document.querySelector('.carousel__lista'), {
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		draggable: true,
+		dots: '.carousel__indicadores',
+		arrows: {
+			prev: '.carousel__anterior',
+			next: '.carousel__siguiente',
+		},
+		responsive: [
+			{
+				// screens greater than >= 775px
+				breakpoint: 600,
+				settings: {
+					// Set to `auto` and provide item width to adjust to viewport
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				// screens greater than >= 1024px
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 4,
+				},
+			},
+		],
+	})
+})
